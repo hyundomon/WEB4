75550522a4a877e5f6e4d860477156ac093a3d2f
hyundomon <103475271+hyundomon@users.noreply.github.com>
2022-07-20 12:46:36 +0900

    java

---
+++ 20220720/26dom요소 가져오기.js
@@ -0,0 +1,13 @@
+const queryClass = document.quenrySelector('.class')
+const queryId = document.quenrySelector('#id')
+// 쿼리셀렉터로 가져오기
+
+const getId = document.getElementsById('id')
+// id로 가져오기
+
+const getClass = document.getElementsByClassName('class')
+// class로 가져오기.
+/*
+class로 가져올 경우는 여러개의 돔요소를 가져오기 때문에
+getClass[0]과 같은 형태로 사용 가능하다
+*/
