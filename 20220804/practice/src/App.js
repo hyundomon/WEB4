c03ff5d2514fd5bc39d2bf0ca0d6d37049eb6ca3
hyundomon <103475271+hyundomon@users.noreply.github.com>
2022-08-04 12:56:38 +0900

    react

---
+++ 20220804/practice/src/App.js
@@ -0,0 +1,14 @@
+import './reset.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> H1 메인타이틀입니다.</h1>
+      <p className="hi">이곳은 p태그 영역입니다.안녕하세요!</p>
+      P.className
+    </div>
+  );
+}
+
+export default App;
